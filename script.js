6f8d253f58b502d921ff7dabeac8a05464d74d99
missing-user <philipp.jurasic@gmx.de>
2020-04-20 15:29:36 +0200

    worker test

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 var canvas = document.getElementById('canvas'),
-	ctx = canvas.getContext('2d')
+	ctx = canvas.getContext('bitmaprenderer')
 elemLeft = canvas.offsetLeft,
 	elemTop = canvas.offsetTop,
 	charges = [],
 	dynamics = [],
 	res = 10,
 	lastTime = 0
-//var offscreen = canvas.transferControlToOffscreen()
+var offscreen = canvas.transferControlToOffscreen()
 const multiplyer = Math.PI * 4,
 	scale = 100,
 	limitSubstep = 100
 var paused = false
-/*var worker = new Worker("worker.js");
-worker.postMessage({
-	canvas: offscreen
-}, [offscreen])*/
+var worker = new Worker("worker.js")
 // Add event listener for `click` events.
 canvas.addEventListener('mousedown', function (event) {
 	var x = event.pageX - elemLeft,
@@ -44,64 +41,34 @@ canvas.addEventListener('mousedown', function (event) {
 canvas.addEventListener("contextmenu", function (event) {
 	console.log('context menu prevented');
 	event.preventDefault();
-});
-
-function loop() {
-	delta = (performance.now() - lastTime) / 1000
-	lastTime = performance.now()
-	if (delta > 0.1) delta = 0.1
-	console.log(delta)
-	dt = delta / limitSubstep
-	for (var i = 0; i < limitSubstep; i++)
-		for (charge of dynamics) {
-			fv = getFieldVector(charge.x, charge.y)
-			charge.vx += fv.x * charge.q * dt
-			charge.vy += fv.y * charge.q * dt
-			charge.x += charge.vx * dt
-			charge.y += charge.vy * dt
-		}
-	for (charge of dynamics) charge.trail.push({
-		x: charge.x,
-		y: charge.y
-	})
-	draw()
-	if (!paused) requestAnimationFrame(loop)
-}
-
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (var i = 0; i < canvas.width; i += res) {
-		for (var j = 0; j < canvas.height; j += res) {
-			ctx.fillStyle = getColor(getFieldStrength(i / scale, j / scale))
-			ctx.fillRect(i, j, res, res);
-		}
+})
+worker.addEventListener('message', function (ev) {
+	if (ev.data.msg === 'render') {
+		ctx.transferFromImageBitmap(ev.data.bitmap);
 	}
-	for (var charge of charges) {
-		ctx.fillStyle = charge.colour;
-		ctx.fillRect(charge.x * scale - 1 + res / 2, charge.y * scale - 1 + res / 2, 2, 2);
-	}
-	ctx.beginPath()
-	for (charge of dynamics) {
-		ctx.moveTo(charge.trail[0].x * scale, charge.trail[0].y * scale)
-		for (var pos of charge.trail) {
-			ctx.lineTo(pos.x * scale + res / 2, pos.y * scale + res / 2)
-		}
-	}
-	ctx.stroke()
-}
+})
+worker.postMessage({
+	dynamics: dynamics,
+	charges: charges,
+	scale: scale,
+	res: res,
+	multiplyer: multiplyer,
+	canvas: offscreen,
+	limitSubstep: limitSubstep
+}, [offscreen])
 
 function updateView() {
-	console.log('highres view update')
-	dynamics = charges.filter((ch) => {
-		return ch.dynamic
-	})
+	worker.postMessage({
+		charges: charges,
+		update: ['charges']
+	}, [offscreen])
 }
 
 function renderHighRes() {
-	res = 1
-	paused = false
-	draw()
-	res = 10
+	worker.postMessage({
+		res: 1,
+		update: ['res']
+	}, [offscreen])
 }
 
 function pause() {
@@ -112,51 +79,3 @@ function pause() {
 		paused = true
 	}
 }
-
-function getFieldVector(x, y) {
-	vec = {
-		x: 0,
-		y: 0
-	}
-	for (var e of charges) {
-		dx = x - e.x
-		dy = y - e.y
-		r2 = dx * dx + dy * dy
-		if (r2 < 0.0005) r2 = 0.0005
-		strength = multiplyer * e.q / r2
-		if (r2 > 0) {
-			dist = Math.sqrt(r2)
-			vec.x += strength * dx / dist
-			vec.y += strength * dy / dist
-		}
-	}
-	return vec
-}
-
-function getFieldStrength(x, y) {
-	strength = 0
-	for (var e of charges) {
-		dx = x - e.x
-		dy = y - e.y
-		r2 = dx * dx + dy * dy
-		if (r2 < 0.0005) r2 = 0.0005
-		strength += multiplyer * e.q / r2
-	}
-	return strength
-}
-
-function getColor(dist, max = 5) {
-	if (dist < 0) {
-		dist = -dist
-		dist = Math.log(dist)
-		if (dist > max) dist = max
-		ratio = dist / max * 255
-		return ['rgb(0,', 255 - ratio, ',', ratio, ')'].join('')
-	} else {
-		dist = Math.log(dist)
-		if (dist > max) dist = max
-		ratio = dist / max * 255
-		return ['rgb(', ratio, ',', 255 - ratio, ',0)'].join('')
-	}
-}
-loop()
--- worker.js
+++ worker.js
@@ -1,47 +1,119 @@
+var canvas
+var ctx
+var scalw
+var res
+var dynamics
+var charges
+var multiplyer
+var limitSubstep
+var lastTime = 0
 onmessage = function (evt) {
-	const canvas = evt.data.canvas;
-	const ctx = canvas.getContext("2d");
-	const scale = evt.data.scale
-	const res = evt.data.res
-	const dynamics = evt.data.dynamics
-	const charges = evt.data.charges
-	const multiplyer = evt.data.multiplyer
+	if ('update' in evt.data)
+		for (var key of evt.data.update) {
+			console.log('updating key', key)
+			this[key] = evt.data[key]
+			dynamics = charges.filter((ch) => {
+				return ch.dynamic
+			})
+		} else {
+			canvas = evt.data.canvas;
+			ctx = canvas.getContext("2d");
+			scale = evt.data.scale
+			res = evt.data.res
+			dynamics = evt.data.dynamics
+			charges = evt.data.charges
+			multiplyer = evt.data.multiplyer
+			limitSubstep = evt.data.limitSubstep
+		}
+	requestAnimationFrame(loop)
+}
 
-	function render(time) {
-		// ... some drawing using the gl context ...
-		console.log(time);
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		for (var i = 0; i < canvas.width; i += res) {
-			for (var j = 0; j < canvas.height; j += res) {
-				ctx.fillStyle = getColor(getFieldStrength(i / scale, j / scale))
-				ctx.fillRect(i, j, res, res);
-			}
-		}
-		for (var charge of charges) {
-			ctx.fillStyle = charge.colour;
-			ctx.fillRect(charge.x * scale - 1 + res / 2, charge.y * scale - 1 + res / 2, 2, 2);
-		}
-		ctx.beginPath()
+function loop() {
+	delta = (performance.now() - lastTime) / 1000
+	lastTime = performance.now()
+	if (delta > 0.1) delta = 0.1
+	console.log(delta)
+	dt = delta / limitSubstep
+	for (var i = 0; i < limitSubstep; i++)
 		for (charge of dynamics) {
-			ctx.moveTo(charge.trail[0].x * scale, charge.trail[0].y * scale)
-			for (var pos of charge.trail) {
-				ctx.lineTo(pos.x * scale, pos.y * scale)
-			}
+			fv = getFieldVector(charge.x, charge.y)
+			charge.vx += fv.x * charge.q * dt
+			charge.vy += fv.y * charge.q * dt
+			charge.x += charge.vx * dt
+			charge.y += charge.vy * dt
 		}
-		ctx.stroke()
-		requestAnimationFrame(render)
-	}
+	for (charge of dynamics) charge.trail.push({
+		x: charge.x,
+		y: charge.y
+	})
+	if (!paused) requestAnimationFrame(loop)
+}
 
-	function getFieldStrength(x, y) {
-		strength = 0
-		for (var e of charges) {
-			dx = x - e.x
-			dy = y - e.y
-			r2 = dx * dx + dy * dy
-			if (r2 < 0.0005) r2 = 0.0005
-			strength += multiplyer * e.q / r2
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (var i = 0; i < canvas.width; i += res) {
+		for (var j = 0; j < canvas.height; j += res) {
+			ctx.fillStyle = getColor(getFieldStrength(i / scale, j / scale))
+			ctx.fillRect(i, j, res, res);
 		}
-		return strength
 	}
-	requestAnimationFrame(render)
+	for (var charge of charges) {
+		ctx.fillStyle = charge.colour;
+		ctx.fillRect(charge.x * scale - 1 + res / 2, charge.y * scale - 1 + res / 2, 2, 2);
+	}
+	ctx.beginPath()
+	for (charge of dynamics) {
+		ctx.moveTo(charge.trail[0].x * scale, charge.trail[0].y * scale)
+		for (var pos of charge.trail) {
+			ctx.lineTo(pos.x * scale, pos.y * scale)
+		}
+	}
+	ctx.stroke()
+}
+
+function getFieldVector(x, y) {
+	vec = {
+		x: 0,
+		y: 0
+	}
+	for (var e of charges) {
+		dx = x - e.x
+		dy = y - e.y
+		r2 = dx * dx + dy * dy
+		if (r2 < 0.0005) r2 = 0.0005
+		strength = multiplyer * e.q / r2
+		if (r2 > 0) {
+			dist = Math.sqrt(r2)
+			vec.x += strength * dx / dist
+			vec.y += strength * dy / dist
+		}
+	}
+	return vec
+}
+
+function getFieldStrength(x, y) {
+	strength = 0
+	for (var e of charges) {
+		dx = x - e.x
+		dy = y - e.y
+		r2 = dx * dx + dy * dy
+		if (r2 < 0.0005) r2 = 0.0005
+		strength += multiplyer * e.q / r2
+	}
+	return strength
+}
+
+function getColor(dist, max = 5) {
+	if (dist < 0) {
+		dist = -dist
+		dist = Math.log(dist)
+		if (dist > max) dist = max
+		ratio = dist / max * 255
+		return ['rgb(0,', 255 - ratio, ',', ratio, ')'].join('')
+	} else {
+		dist = Math.log(dist)
+		if (dist > max) dist = max
+		ratio = dist / max * 255
+		return ['rgb(', ratio, ',', 255 - ratio, ',0)'].join('')
+	}
 }
